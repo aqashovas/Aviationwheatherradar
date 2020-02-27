44e1af50d21b2fbcb4fa468ab6ea87ba3d275f57
Sabina Aqashova <aqashova@inbox.ru>
2020-02-27 22:38:19 +0400

    #

---
+++ kursiis/Form1.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace kursiis
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.btnOk = new System.Windows.Forms.Button();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.pnlplane = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.timer3 = new System.Windows.Forms.Timer(this.components);
+            this.timer4 = new System.Windows.Forms.Timer(this.components);
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pnlplane)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(513, -1);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(100, 96);
+            this.richTextBox1.TabIndex = 45;
+            this.richTextBox1.Text = "";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 50;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(698, 29);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 47;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnOk
+            // 
+            this.btnOk.Location = new System.Drawing.Point(698, 72);
+            this.btnOk.Name = "btnOk";
+            this.btnOk.Size = new System.Drawing.Size(75, 23);
+            this.btnOk.TabIndex = 46;
+            this.btnOk.Text = "ok";
+            this.btnOk.UseVisualStyleBackColor = true;
+            this.btnOk.Click += new System.EventHandler(this.btnOk_Click);
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 50;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // pnlplane
+            // 
+            this.pnlplane.BackColor = System.Drawing.Color.Transparent;
+            this.pnlplane.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pnlplane.BackgroundImage")));
+            this.pnlplane.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pnlplane.Location = new System.Drawing.Point(956, 431);
+            this.pnlplane.Name = "pnlplane";
+            this.pnlplane.Size = new System.Drawing.Size(37, 30);
+            this.pnlplane.TabIndex = 48;
+            this.pnlplane.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Image = global::kursiis.Properties.Resources.map;
+            this.pictureBox1.Location = new System.Drawing.Point(-1, -1);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(614, 524);
+            this.pictureBox1.TabIndex = 44;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            this.pictureBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseClick);
+            // 
+            // timer3
+            // 
+            this.timer3.Interval = 50;
+            this.timer3.Tick += new System.EventHandler(this.timer3_Tick);
+            // 
+            // timer4
+            // 
+            this.timer4.Interval = 50;
+            this.timer4.Tick += new System.EventHandler(this.timer4_Tick);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(716, 116);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 49;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(614, 749);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnOk);
+            this.Controls.Add(this.pnlplane);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pnlplane)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnOk;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.PictureBox pnlplane;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Timer timer3;
+        private System.Windows.Forms.Timer timer4;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ kursiis/Form1.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace kursiis
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            pictureBox1.Controls.Add(pnlplane);
+            //test();
+
+
+        }
+        
+        public double Xcoor = 6371 * Math.Cos(38.28482222 * 3.14 / 180) * Math.Cos(44.96833333 * 3.14 / 180);
+        public double Ycoor = 6371 * Math.Cos(38.28482222 * 3.14 / 180) * Math.Sin(44.96833333 * 3.14 / 180);
+        public List<Point> pnts = new List<Point>();
+        public List<Point> pntskorr = new List<Point>();
+        public int i = 1, j = 1, k = 0, l = 1;
+        public bool draw = false;
+        public int x1, x2, y1, y2, x3, x4, y3, y4, x5, x6, y5, y6;
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            timer3.Enabled = true;
+            timer3.Start();
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            Stop();
+        }
+
+        private void timer4_Tick(object sender, EventArgs e)
+        {
+            Stop2();
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+            foreach (Point point in pnts)
+            {
+                e.Graphics.FillEllipse(Brushes.Red, point.X - 3, point.Y - 3, 10, 10);
+                if (pnts.Count < 2)
+                {
+                    return;
+                }
+                e.Graphics.DrawCurve(Pens.Black, pnts.ToArray());
+                e.Graphics.Save();
+            }
+            e.Graphics.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+            foreach (Point point in pntskorr)
+            {
+                e.Graphics.FillEllipse(Brushes.Red, point.X - 3, point.Y - 3, 10, 10);
+                if (pntskorr.Count < 2)
+                {
+                    return;
+                }
+                e.Graphics.DrawCurve(Pens.Black, pntskorr.ToArray());
+                e.Graphics.Save();
+            }
+        }
+
+        private void pictureBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            MessageBox.Show(e.Location.ToString());
+            k++;
+            if (draw == false && k < 3)
+            {
+                pnts.Add(e.Location);
+                MessageBox.Show(e.Location.ToString());
+            }
+            else if (draw == true && k > 2)
+            {
+                pntskorr.Add(e.Location);
+                MessageBox.Show(pntskorr.Count.ToString());
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            i++;
+
+            richTextBox1.Clear();
+
+            double y = Convert.ToDouble(pnlplane.Location.Y) + Ycoor - 276 /*+ pictureBox1.Height / 2*/;
+            double x = Convert.ToDouble(pnlplane.Location.X) + Xcoor /*+ pictureBox1.Width / 2*/;
+            double coord = Math.Atan2(y, x);
+            double coord2 = Math.Atan2(x, y);
+            coord2 = coord2 * 180.0 / Math.PI;
+            coord = coord * 180.0 / Math.PI;
+            double minutes = (coord - Math.Floor(coord)) * 60.0;
+            double seconds = (minutes - Math.Floor(minutes)) * 60.0;
+            double minutes2 = (coord2 - Math.Floor(coord2)) * 60.0;
+            double seconds2 = (minutes2 - Math.Floor(minutes2)) * 60.0;
+            //int sec = (int)Math.Round(coord * 3600);
+            //int deg = sec / 3600;
+            //sec = Math.Abs(sec % 3600);
+            //int min = sec / 60;
+            //sec %= 60;
+            //textBox1.Text =(Convert.ToDouble(pictureBox1.Location.X)+Xcoor).ToString()+" "+ (Convert.ToDouble(pictureBox1.Location.Y) + Ycoor-660).ToString();
+            richTextBox1.Text = "Lat=" + Math.Round(coord, 2).ToString() + "°" + Math.Round(minutes, 2).ToString() + "''" + Math.Round(seconds, 2).ToString() + "'" + Environment.NewLine + "Long=" + Math.Round(coord2, 2).ToString() + "°" + Math.Round(minutes2, 2).ToString() + "''" + Math.Round(seconds2, 2).ToString() + "'";
+            x1 = pnts.First().X - pnlplane.Width / 2;
+            x2 = pnts.Last().X - pnlplane.Width / 2;
+            y1 = pnts.First().Y;
+            y2 = pnts.Last().Y;
+            x1 = x1 + (x2 - x1) * i / 500;
+            y1 = y1 + (y2 - y1) * i / 500;
+            pnlplane.Location = new Point(x1, y1);
+        }
+        public void test()
+        {
+            if (pntskorr.Count > 1)
+            {
+                timer3.Enabled = true;
+                timer3.Start();
+            }
+        }
+        public void Stop()
+        {
+            if ((pnlplane.Location.X == x2 && pnlplane.Location.Y == y2) || (draw == true && k > 2))
+            {
+                timer1.Enabled = false;
+                timer2.Enabled = false;
+                timer1.Stop();
+                MessageBox.Show("ubec");
+              
+            }
+            // timer1.Stop();
+        }
+        public void Stop2()
+        {
+            if (pnlplane.Location.X == x4 && pnlplane.Location.Y == y4)
+            {
+                timer3.Enabled = false;
+                timer3.Stop();
+                timer4.Enabled = false;
+
+
+            }
+            // timer1.Stop();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            draw = true;
+        }
+
+
+
+        private void btnOk_Click(object sender, EventArgs e)
+        {
+
+
+            timer1.Enabled = true;
+            timer1.Start();
+            timer2.Enabled = true;
+            timer2.Start();
+        }
+
+       
+
+
+        private void timer3_Tick(object sender, EventArgs e)
+        {
+            //MessageBox.Show("vvjgv");
+            j++;
+
+            richTextBox1.Clear();
+
+            double y = Convert.ToDouble(pnlplane.Location.Y) + Ycoor - 276 /*+ pictureBox1.Height / 2*/;
+            double x = Convert.ToDouble(pnlplane.Location.X) + Xcoor /*+ pictureBox1.Width / 2*/;
+            double coord = Math.Atan2(y, x);
+            double coord2 = Math.Atan2(x, y);
+            coord2 = coord2 * 180.0 / Math.PI;
+            coord = coord * 180.0 / Math.PI;
+            double minutes = (coord - Math.Floor(coord)) * 60.0;
+            double seconds = (minutes - Math.Floor(minutes)) * 60.0;
+            double minutes2 = (coord2 - Math.Floor(coord2)) * 60.0;
+            double seconds2 = (minutes2 - Math.Floor(minutes2)) * 60.0;
+            //int sec = (int)Math.Round(coord * 3600);
+            //int deg = sec / 3600;
+            //sec = Math.Abs(sec % 3600);
+            //int min = sec / 60;
+            //sec %= 60;
+            //textBox1.Text =(Convert.ToDouble(pictureBox1.Location.X)+Xcoor).ToString()+" "+ (Convert.ToDouble(pictureBox1.Location.Y) + Ycoor-660).ToString();
+            richTextBox1.Text = "Lat=" + Math.Round(coord, 2).ToString() + "°" + Math.Round(minutes, 2).ToString() + "''" + Math.Round(seconds, 2).ToString() + "'" + Environment.NewLine + "Long=" + Math.Round(coord2, 2).ToString() + "°" + Math.Round(minutes2, 2).ToString() + "''" + Math.Round(seconds2, 2).ToString() + "'";
+            x3 = pntskorr.First().X - pnlplane.Width / 2;
+            x4 = pntskorr.Last().X - pnlplane.Width / 2;
+            y3 = pntskorr.First().Y;
+            y4 = pntskorr.Last().Y;
+            x3 = x3 + (x4 - x3) * j / 500;
+            y3 = y3 + (y4 - y3) * j / 500;
+            pnlplane.Location = new Point(x3, y3);
+        }
+
+    }
+    
+}
